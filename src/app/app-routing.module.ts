ec0c1372bfda70f992c1b99e6a4ca169114545df
Smit Chavda <chavda.smit52@gmail.com>
2024-05-09 23:36:42 +0530

    added layout in project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Site routes goes here
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [{ path: '', component: AppComponent, pathMatch: 'full' }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
